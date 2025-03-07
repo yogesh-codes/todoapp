15001b9a58c329a5dae33527e4353890367f0343
Yogesh <139562574+yogesh-codes@users.noreply.github.com>
2025-03-07 15:16:08 +0530

    delete unnecessary utils

--- src/utils/validate.py
+++
@@ -1,7 +0,0 @@
-def isPositiveInt(value):
-    if not isinstance(value, int):
-        raise ValueError(f"unit value Input must be an integer.")
-    if value < 0:
-        raise ValueError("units value Input cannot be negative.")
-    return value
-    
